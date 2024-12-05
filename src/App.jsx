4c8ed286e0992ef5748592f2ecbd7f8837169f75
monajstha <manojstha700@gmail.com>
2024-12-05 13:54:41 +0545

    add: add toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { ContextProvider } from "./context/context";
+import { RouterProvider } from "react-router";
+import router from "./routes/router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <p>Hello</p>
-    </>
+    <ContextProvider>
+      <RouterProvider router={router}>
+        <ToastContainer position="top-right" />
+      </RouterProvider>
+    </ContextProvider>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ContextProvider } from "./context/context";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ContextProvider>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
-    </ContextProvider>
+    <App />
   </StrictMode>
 );
